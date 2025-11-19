4d80734fac44e56d82e67e242c74120835504df2
Nick <NickyDaB@gmail.com>
2025-11-18 21:26:23 -0500

    working

--- practicing-file-io/practicing-file-io/Program.cs
+++ practicing-file-io/practicing-file-io/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            FileStream stream = File.Create("my_badass_file.txt");
+
+            stream.Close();
+
+            File.AppendAllText("my_badass_file.txt", "I am the programmer, and I say let there be text in this badass file!");
         }
     }
 }
